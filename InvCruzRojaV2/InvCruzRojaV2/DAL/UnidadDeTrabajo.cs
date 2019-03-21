803cf32921767f6738492d1b2f525ae13de4fb12
CharlieGuzman <43427108+CharlieGuzman@users.noreply.github.com>
2019-03-20 18:27:08 -0600

    Proyecto

---
+++ InvCruzRojaV2/InvCruzRojaV2/DAL/UnidadDeTrabajo.cs
@@ -0,0 +1,34 @@
+﻿using BackEnd.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.DAL
+{
+    public class UnidadDeTrabajo<T> : IDisposable where T : class
+    {
+        private readonly CruzRojaInventarios_V2Entities context;
+        public IDALGenerico<T> genericDAL;
+
+
+        public UnidadDeTrabajo(CruzRojaInventarios_V2Entities _context)
+        {
+            context = _context;
+            genericDAL = new DALGenericoImpl<T>(context);
+
+        }
+
+        public void Complete()
+        {
+            context.SaveChanges();
+        }
+
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
